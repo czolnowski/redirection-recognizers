49ccc7dff2c903b318f5e0551c9f049bd32eafa9
Marcin Czołnowski <marcin@czolnowski.net>
2015-07-26 10:07:35 +0200

    Initial commit

---
+++ src/Mindweb/RedirectionRecognizers/FromBase64RedirectionRecognizer.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mindweb\RedirectionRecognizers;
+
+use Mindweb\Recognizer\Event;
+use Mindweb\Recognizer\Recognizer;
+
+class FromBase64RedirectionRecognizer extends Recognizer
+{
+    /**
+     * @param Event\AttributionEvent $attributionEvent
+     */
+    public function recognize(Event\AttributionEvent $attributionEvent)
+    {
+    }
+}
